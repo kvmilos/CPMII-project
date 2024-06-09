d50e8c9f71d0c7d400d87ed2b0fb343e165c8fb6
Adam Okrasiński <a.j.okrasinski@gmail.com>
2024-06-09 23:25:14 +0200

    syncing the branches

--- screen_pygame.py
+++ screen_pygame.py
@@ -1,11 +1,15 @@
+import neat.config
 import pygame
 import pymunk.pygame_util
 from simulation_pymunk import Constants, Simulation
+import neat
+import os
 
     #TODO: (optional) Add some 2D top-down sprites for the robot, food, enemies
     #TODO: (optional) Add sounds
 
     #TODO: Train an AI to play the game
+agent_counter = 0
 
 class Screen():
     # class with the window, to display
@@ -14,63 +18,151 @@ class Screen():
         pygame.display.set_caption("CPM II: Robot Simulation")
         self.window = pygame.display.set_mode((Constants.WIDTH, Constants.HEIGHT))
         self.window.fill("white")
-        self.font = pygame.font.SysFont("Arial", 30)
+        self.font = pygame.font.SysFont("Arial", 24)
 
     # function to run the pygame window
-    def run(self, simulation):
+    def run(self, genomes, config):
+        #global  environments, robots, ge, nets, simulations
+        
         window = self.window
         run = True
         clock = pygame.time.Clock()
         fps = 60
-        dt = 1/fps 
+        dt = 10/fps 
+        
+        #simulation = Simulation()
+        #space = simulation.run()
+        #robot = simulation.get_robot()
+        #_ = robot.draw(self.window)
+        
+        environments = []
+        simulations = []
+        robots = []
+        ge = []
+        nets = []
 
-        space, robot = simulation.run()
-        _ = robot.draw(self.window)
+        simulation = Simulation()
+        simulations.append(simulation)
+        space = simulation.run(2, n_robots=10)
+        robots = simulation.get_robots()
+       
+        for genome_id, genome in genomes:
 
+            environments.append(space)
+            #robots.append(robot)
+            ge.append(genome)
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            nets.append(net)
+            genome.fitness = 0
+
+        global agent_counter
+        agent_counter += 1
+        
+        #if agent_counter%10 == 0:
+           # return
+        
+        start_ticks = pygame.time.get_ticks()
         while run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     run = False
-
-            # move the robot using wasd
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_w]:
-                robot.move(2)
-            if keys[pygame.K_s]:
-                robot.move(-2)
-            if keys[pygame.K_a]:
-                robot.rotate(-1)
-            if keys[pygame.K_d]:
-                robot.rotate(1)
-
+                    break
             self.window.fill("white")
-            sensors = robot.draw(self.window)
-
+            for robot in robots:
+                _ = robot.draw(self.window)
+                #sensors = robot.draw(self.window)
+            
+            
             # update points every frame
             simulation.update_points(dt)
-            self.display_points(simulation.points)
-            
-            # TODO: where to put this? also, what exact info do we need to output?
+            simulation.update_position(dt)
+
+           # simulation.check_if_stuck()
+
+            self.display_points(simulation.points) 
+            self.display_generation(agent_counter)         
+           # TODO: where to put this? also, what exact info do we need to output?
             # # info for future training (?)
             # info = [robot.body.velocity.x, robot.body.velocity.y, sensors]
             # print(info) 
 
             # move the enemies
-            simulation.move_enemies()
+            #simulation.move_enemies()
 
             pygame.display.update()
             space.step(dt)
             clock.tick(fps)
 
-        pygame.quit()
-    
+                
+            seconds = (pygame.time.get_ticks() - start_ticks)/1000
+            if seconds > 20:
+                break
+            for robot in robots:
+                index = robots.index(robot)
+                #robot = simulation.get_robot()
+                data = robot.get_data()
+                output = nets[index].activate(data)
+                choice = output.index(max(output))
+                if choice == 0:
+                    robot.move(0.1)
+                elif choice == 1:
+                    robot.rotate(1)
+                else:
+                    robot.rotate(-1)
+            
+            #print(len(genomes))
+            for robot in robots:
+                index = robots.index(robot)
+                tmp = genomes[index][1].fitness
+
+                tmp += simulation.points[index]
+        #print("TMP new", tmp)
+        #print()
+                ge[index].fitness += simulation.points[index]/100
+        
+            #print('GENOMES:', genomes[index][1].fitness)
+        for robot in robots:
+            index = robots.index(robot)
+            print(ge[index].fitness)
+        print()
+        #print(len(genomes))
+        #print(simulation.points)
+    #pygame.quit()
+
     def display_points(self, points):
         # render the points
-        points_surface = self.font.render(f"Points: {points}", True, "black")
-        self.window.blit(points_surface, (10, 10))
+        points_surface = []
+        points_surface = self.font.render("Points:", True, "white")
+        self.window.blit(points_surface, (35, 5))
+    
+        for i in range(0, len(points)):
+            points_surface = self.font.render(f"{points[i]}", True, (Constants.COLORS2[i]))
+            self.window.blit(points_surface, (50*(i%5)+50, 30*int(i/5)+30))
+    
+    def display_generation(self, agent_counter):
+        points_surface = self.font.render(f"Generation: {agent_counter}", True, "white")
+        self.window.blit(points_surface, (570, 5))
 
+def ai_run(config_path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+        )
+    pop = neat.Population(config)
+    #simulation = Simulation()
+    window = Screen()
+    lol = pop.run(window.run, 1000)
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    best_genome = pop.best_genome
+    print('Best fitness: {0:3.5f} - size: {1!r}'.format(best_genome.fitness, best_genome.size()))
 
 if __name__ == "__main__":
-    simulation = Simulation()
-    window = Screen()
-    window.run(simulation)
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    ai_run(config_path)
+    
--- simulation_pymunk.py
+++ simulation_pymunk.py
@@ -15,8 +15,8 @@ class Simulation():
         self.space = pymunk.Space()
         self.space.gravity = (0.0, 0.0)
         self.space.iterations = 30
-
-        self.robot = Robot("robot", self.space, (Constants.WIDTH // 2, Constants.HEIGHT // 2))
+        self.seed = random.Random()
+        self.walls = self.spawn_random_walls()
 
         for cords in Constants.WALLS:
             wall = Wall(self.space, *cords)
@@ -35,7 +35,21 @@ class Simulation():
         enemy_handler = self.space.add_collision_handler(Constants.ROBOT, Constants.ENEMY)
         enemy_handler.begin = self.robot_hit_enemy
 
-        return self.space, self.robot
+        #robot-obstacles
+        #wall_handler = self.space.add_collision_handler(Constants.ROBOT, Constants.WALL)
+        #wall_handler.begin = self.robot_hit_wall
+        
+        #robot_rect = self.robot.get_rect(topleft = self.robot.body.position)
+
+        return self.space
+
+    def get_robots(self):
+        return self.robots
+    
+    def spawn_robot(self, color):
+        self.robots.append(Robot("robot", self.space, (Constants.WIDTH // 2, Constants.HEIGHT // 2), color=color))
+        self.robots_previous_positions.append([(100, 100)])
+        self.food.append([])
 
     def spawn_random_walls(self, n=5):
         # function to add random walls
@@ -48,23 +62,32 @@ class Simulation():
 
     def spawn_food(self, shift):
         # function to add random food
-        while True:
+        #if len(self.food) == 0:
+        #while True:
+            for robot in self.robots:
+                index = self.robots.index(robot)
+                if self.food[index] == []:
             # generate random position in between walls
-            x = random.randint(Constants.WALLS_DISTANCE + shift, Constants.WIDTH - Constants.WALLS_DISTANCE - shift)
-            y = random.randint(Constants.WALLS_DISTANCE + shift, Constants.HEIGHT - Constants.WALLS_DISTANCE - shift)
+                    x = random.randint(Constants.WALLS_DISTANCE + shift, Constants.WIDTH - Constants.WALLS_DISTANCE - shift)
+                    y = random.randint(Constants.WALLS_DISTANCE + shift, Constants.HEIGHT - Constants.WALLS_DISTANCE - shift)
 
             # create a temp body to check for overlap
-            temp_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-            temp_body.position = (x, y)
-            temp_shape = pymunk.Circle(temp_body, Constants.FOOD_SIZE)
+                    temp_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+                    temp_body.position = (x, y)
+                    temp_shape = pymunk.Circle(temp_body, Constants.FOOD_SIZE)
 
             # check for overlap
-            overlap = self.space.shape_query(temp_shape)
+                    overlap = self.space.shape_query(temp_shape)
 
             # if no overlap, add the food
-            if not overlap:
-                Food(self.space, (x, y))
-                break
+                    if not overlap:
+                #Food(self.space, (x, y))
+            #for robot in self.robots:
+              #  index = self.robots.index(robot)
+              #  if self.food[index] == []:
+                        self.food[index].append(Food(self.space, (x, y), Constants.COLORS2[index]))
+            #print(self.food[0])
+                    
 
     def spawn_enemy(self, shift):
         # function to add random enemy
@@ -92,15 +115,29 @@ class Simulation():
 
     def robot_eat_food(self, arbiter, space, data):
         # remove the food from the space
+        index = 0
         food_shape = arbiter.shapes[1]
-        space.remove(food_shape, food_shape.body)
+        for robot in self.robots:
+            if robot.shape == arbiter.shapes[0]:
+                index = self.robots.index(robot)
+                #print(self.food[index])
+                if robot.shape.color == food_shape.color and food_shape.body.position == self.food[index][0].body.position:
+                    del self.food[index][0]
+                    space.remove(food_shape, food_shape.body)
+                    self.points[index] += Constants.POINTS_GAINED_PER_FOOD
+       # if len(self.food) > 0:
+          #  tmp = []
+        #    for food in self.food:
+          #      tmp.append(food[0].body.position)
+          #  if food_shape.body.position in tmp:
+            #    del self.food[tmp.index(food_shape.body.position)]
 
+        
         # add a new food in a random position
         self.spawn_food(10)
 
         # increase the points
-        self.points += Constants.POINTS_GAINED_PER_FOOD
-
+        
         return False
 
     def robot_hit_enemy(self, arbiter, space, data):
@@ -123,6 +160,14 @@ class Simulation():
 
         return True
 
+    def robot_hit_wall(self, arbiter, space, data):
+         # Decrease points by 5
+        #if self.points - 5 >= 0:
+        self.points[self.robots.index[arbiter[0]]] -= 100
+        #else:
+         #   self.points = 0
+        return True
+
     def move_enemies(self):
         for enemy in self.enemies:
             direction = self.robot.body.position - enemy.body.position
@@ -135,8 +180,35 @@ class Simulation():
         # decrease a point every X seconds
         self.timer += dt
         if self.timer >= Constants.PER_WHAT_TIME_POINTS_ARE_LOST:
-            if self.points - Constants.POINTS_LOST_PER_TIME >= 0:
-                self.points -= Constants.POINTS_LOST_PER_TIME
-            else:
-                self.points = 0
-            self.timer = 0
+            #if self.points - Constants.POINTS_LOST_PER_TIME >= 0:
+            for robot in self.robots:
+                index = self.robots.index(robot)
+                #print(self.points[index])
+                self.points[index] -= Constants.POINTS_LOST_PER_TIME
+            #else:
+                #self.points = 0
+                tmp = []
+                for j, radar in enumerate(self.robots[index].radars):
+                    tmp.append(radar[2])
+                if set(tmp) == {2}:
+                    self.points[index] -= 1
+                elif 3 in set(tmp):
+                    self.points[index] += 1
+                self.timer = 0
+
+    def update_position(self, dt):
+        self.timer2 += dt
+        if self.timer2 >= Constants.PER_WHAT_TIME_POINTS_ARE_LOST/2:
+            for robot in self.robots:
+                index = self.robots.index(robot)
+                self.robots_previous_positions[index].append(self.robots[index].body.position)
+                self.timer2 = 0
+                self.check_if_stuck(index)
+                if len(self.robots_previous_positions[index]) == 5:
+                    del self.robots_previous_positions[index][0]
+            #print(self.robots_previous_positions)
+            
+
+    def check_if_stuck(self, id):
+        if(len(set(self.robots_previous_positions[id])) == 1):
+            self.points[id] -= Constants.POINTS_LOST_PER_NOT_MOVING
